2efc65e1ab253dbe5d50edbe360702aee7db1c0c
KARLA JULIANE SANTOS MEIRA <j.karla@academico.ifrn.edu.br>
2023-11-01 11:14:32 -0300

    otimização das animações OK

--- projeto 3d/Assets/Scripts/Player.cs
+++ projeto 3d/Assets/Scripts/Player.cs
@@ -17,6 +17,8 @@ public class Player : MonoBehaviour
 
     Vector3 moveDirection;
 
+    private bool isWalking;
+
     public float smoothRotTime;
     private float turnSmoothVelocity;
 
@@ -49,20 +51,35 @@ public class Player : MonoBehaviour
 
             if (direction.magnitude > 0)
             {
-                float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-                float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
+
+                if (!anim.GetBool("attacking"))
+                {
+                    float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                    float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
             
-                transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+                    transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
                 
-                moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward * speed;
+                    moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward * speed;
                 
-                anim.SetInteger("transition", 1);
+                    anim.SetInteger("transition", 1);
+
+                    isWalking = true;
+                }
+                else
+                {
+                    anim.SetBool("walking", false);
+                    moveDirection = Vector3.zero;
+                }
+               
                 
             }
-            else
+            else if(isWalking)
             {
+                anim.SetBool("walking", false);
                 anim.SetInteger("transition", 0);
                 moveDirection = Vector3.zero;
+
+                isWalking = false;
             }
         }
 
@@ -78,13 +95,26 @@ public class Player : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                StartCoroutine("Attack");
+
+                if (anim.GetBool("walking"))
+                {
+                    anim.SetBool("walking", false);
+                    anim.SetInteger("transition", 0);
+                }
+
+                if (!anim.GetBool("walking"))
+                {
+                    StartCoroutine("Attack");
+                }
+                
+                
             }
         }
     }
 
     IEnumerator Attack()
     {
+        anim.SetBool("attacking", true);
         anim.SetInteger("transition", 2);
             
         yield return new WaitForSeconds(0.4f);
@@ -99,6 +129,7 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(1f);
         
         anim.SetInteger("transition", 0);
+        anim.SetBool("attacking", false);
     }
 
     void GetEnemiesList()
